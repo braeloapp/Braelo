b0f60313681857fbae6c943571a21d080f36f961
Muhammad Faizan <77250133+faizan0273@users.noreply.github.com>
2026-03-17 22:43:28 +0500

    Add home view to return a running message

--- config/urls.py
+++ config/urls.py
@@ -12,7 +12,11 @@ https://docs.djangoproject.com/en/4.2/topics/http/urls/
 '''
 
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path, include
+
+def home(request):
+    return HttpResponse("Braelo is running.")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
