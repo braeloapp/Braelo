9080e48a978d47a61dcd7a7be134327cee814234
Muhammad Faizan <immuhammadfaizan@gmail.com>
2026-04-03 12:23:49 +0500

    Fix: persist sqlite DB in /home

--- config/settings.py
+++ config/settings.py
@@ -279,7 +279,7 @@ ASGI_APPLICATION = 'config.asgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join('/home/site/wwwroot', 'db.sqlite3'),
     }
 }
 
