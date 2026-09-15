8395c681c3cb8cb1fa75e3acefcf53cba287c4b2
Muhammad zayan ali <muhammadzayanali84@gmail.com>
2026-09-15 18:30:08 +0500

    docs: add comment to AdminPanelConfig class for clarity
    Included a descriptive comment in the AdminPanelConfig class to enhance understanding of its purpose within the admin panel application.

--- admin_panel/apps.py
+++ admin_panel/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
-
+# AdminPanelConfig is the configuration for the admin panel app
 class AdminPanelConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "admin_panel"
